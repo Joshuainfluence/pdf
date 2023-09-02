905c69fc2939bff80840cf9741bdba2e4bad55ac
joshuainfluence <joshuajulius2030@gmail.com>
2023-09-02 10:28:31 +0100

    successful

---
+++ pdf/content.php
@@ -0,0 +1,13 @@
+<?php
+$c = " How are you doing my fellow devs?";
+?>
+
+<div style='text-align:center;'>
+   <?= $c?>
+    <br>
+    <div class="woman">
+        <img src="woman.jpg" alt="">
+        <h1>Hello dear, How are you doing?</h1>
+    </div>
+
+</div>
